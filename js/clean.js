581b3f795806dad52f8d0b885bc3c43be4068b9c
Pyth0nK1d <29705460+Pyth0nK1d@users.noreply.github.com>
2026-02-16 19:57:18 +0100

    Update clean.js

--- js/clean.js
+++ js/clean.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () { const el = document.getElementById("watermark"); if (el) el.remove(); });
+
 window.onload = function () {
   const el = document.getElementById("watermark");
   if (el) el.remove();
